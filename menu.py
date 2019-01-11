b4757aa89d33d1edfccfab1a1d80de4ac05a0c95
mirs1805soft <mirs1805soft@gmail.com>
2019-01-11 14:32:09 +0900

    14:32

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ import json
 
 def scdl_finish():
     scdl_finish_btn.destroy()
+    scdl_cancel_btn.destroy()
 
     scdl_btn = tk.Button(root, text="スケジュール編集", font=("", 25), command=scdl_year)
     scdl_btn.pack(pady=250)#(anchor = "n")
@@ -14,53 +15,59 @@ def scdl_finish():
 
 def scdl_cancel():
     scdl_finish_btn.destroy()
+    scdl_cancel_btn.destroy()
 
-    scdl_btn = tk.Button(root, text="スケジュール編集", font=("", 25), command=scdl_year)
+    #scdl_btn = tk.Button(root, text="スケジュール編集", font=("", 25), command=scdl_year)
     scdl_btn.pack(pady=250)#(anchor = "n")
 
-    talk_btn = tk.Button(root, text="ロボメイトと会話", font=("", 25), command=talk)
+    #talk_btn = tk.Button(root, text="ロボメイトと会話", font=("", 25), command=talk)
     talk_btn.pack()#(anchor = "n")
 
 def scdl_mon_finish():
-    scdl_mon_btn1.destroy()
-    scdl_mon_btn2.destroy()
-    scdl_mon_btn3.destroy()
+    scdl_mon2_btn1.destroy()
+    scdl_mon2_btn2.destroy()
+    scdl_mon2_btn3.destroy()
 
-    scdl_finish_btn.pack()
+    scdl_finish_btn.pack(pady=250)
+    scdl_cancel_btn.pack()
 
 def scdl_tue_finish():
     scdl_tue2_btn1.destroy()
     scdl_tue2_btn2.destroy()
     scdl_tue2_btn3.destroy()
 
-    scdl_finish_btn.pack()
+    scdl_finish_btn.pack(pady=250)
+    scdl_cancel_btn.pack()
 
 def scdl_wed_finish():
     scdl_wed2_btn1.destroy()
     scdl_wed2_btn2.destroy()
 
-    scdl_finish_btn.pack()
+    scdl_finish_btn.pack(pady=250)
+    scdl_cancel_btn.pack()
 
 def scdl_thu_finish():
     scdl_thu2_btn1.destroy()
     scdl_thu2_btn2.destroy()
     scdl_thu2_btn3.destroy()
-    scdl_thu2_btn3.destroy()
+    scdl_thu2_btn4.destroy()
 
-    scdl_finish_btn.pack()
+    scdl_finish_btn.pack(pady=250)
+    scdl_cancel_btn.pack()
 
 def scdl_fri_finish():
     scdl_fri2_btn1.destroy()
     scdl_fri2_btn2.destroy()
     scdl_fri2_btn3.destroy()
-    scdl_fri2_btn3.destroy()
+    scdl_fri2_btn4.destroy()
 
-    scdl_finish_btn.pack()
+    scdl_finish_btn.pack(pady=250)
+    scdl_cancel_btn.pack()
 
 def scdl_mon2():
     scdl_day2_btn_mon.destroy()
     scdl_day2_btn_tue.destroy()
-    scdl_day2_btn_wen.destroy()
+    scdl_day2_btn_wed.destroy()
     scdl_day2_btn_thu.destroy()
     scdl_day2_btn_fri.destroy()
 
@@ -71,7 +78,7 @@ def scdl_mon2():
 def scdl_tue2():
     scdl_day2_btn_mon.destroy()
     scdl_day2_btn_tue.destroy()
-    scdl_day2_btn_wen.destroy()
+    scdl_day2_btn_wed.destroy()
     scdl_day2_btn_thu.destroy()
     scdl_day2_btn_fri.destroy()
 
@@ -79,20 +86,20 @@ def scdl_tue2():
     scdl_tue2_btn2.pack(pady=10)
     scdl_tue2_btn3.pack(pady=10)
 
-def scdl_wen2():
+def scdl_wed2():
     scdl_day2_btn_mon.destroy()
     scdl_day2_btn_tue.destroy()
-    scdl_day2_btn_wen.destroy()
+    scdl_day2_btn_wed.destroy()
     scdl_day2_btn_thu.destroy()
     scdl_day2_btn_fri.destroy()
 
-    scdl_wen2_btn1.pack(pady=10)
-    scdl_wen2_btn2.pack(pady=10)
+    scdl_wed2_btn1.pack(pady=10)
+    scdl_wed2_btn2.pack(pady=10)
 
 def scdl_thu2():
     scdl_day2_btn_mon.destroy()
     scdl_day2_btn_tue.destroy()
-    scdl_day2_btn_wen.destroy()
+    scdl_day2_btn_wed.destroy()
     scdl_day2_btn_thu.destroy()
     scdl_day2_btn_fri.destroy()
 
@@ -104,7 +111,7 @@ def scdl_thu2():
 def scdl_fri2():
     scdl_day2_btn_mon.destroy()
     scdl_day2_btn_tue.destroy()
-    scdl_day2_btn_wen.destroy()
+    scdl_day2_btn_wed.destroy()
     scdl_day2_btn_thu.destroy()
     scdl_day2_btn_fri.destroy()
 
@@ -119,13 +126,13 @@ def scdl_dayofweek2():
 
     scdl_day2_btn_mon.pack(pady=10)
     scdl_day2_btn_tue.pack(pady=10)
-    scdl_day2_btn_wen.pack(pady=10)
+    scdl_day2_btn_wed.pack(pady=10)
     scdl_day2_btn_thu.pack(pady=10)
     scdl_day2_btn_fri.pack(pady=10)
 
 def scdl_day2():
-    scdl_month_etr.destry()
-    scdl_month_etr.destry()
+    scdl_month2_etr.destroy()
+    scdl_month2_btn.destroy()
 
     scdl_day2_etr.pack(pady=250)
     scdl_day2_btn.pack()
@@ -138,9 +145,9 @@ def scdl_month2():
     scdl_month2_btn.pack()
 
 def scdl_year2_mon():
-    scdl_mon_btn1.destroy()
-    scdl_mon_btn2.destroy()
-    scdl_mon_btn3.destroy()
+    scdl_mon2_btn1.destroy()
+    scdl_mon2_btn2.destroy()
+    scdl_mon2_btn3.destroy()
 
     scdl_year2_lbl.pack(pady=250)
     scdl_year2_btn.pack()
@@ -164,7 +171,7 @@ def scdl_year2_thu():
     scdl_thu_btn1.destroy()
     scdl_thu_btn2.destroy()
     scdl_thu_btn3.destroy()
-    scdl_thu_btn3.destroy()
+    scdl_thu_btn4.destroy()
 
     scdl_year2_lbl.pack(pady=250)
     scdl_year2_btn.pack()
@@ -200,15 +207,15 @@ def scdl_tue():
     scdl_tue_btn2.pack(pady=10)
     scdl_tue_btn3.pack(pady=10)
 
-def scdl_wen():
+def scdl_wed():
     scdl_day_btn_mon.destroy()
     scdl_day_btn_tue.destroy()
     scdl_day_btn_wen.destroy()
     scdl_day_btn_thu.destroy()
     scdl_day_btn_fri.destroy()
 
-    scdl_wen_btn1.pack(pady=10)
-    scdl_wen_btn2.pack(pady=10)
+    scdl_wed_btn1.pack(pady=10)
+    scdl_wed_btn2.pack(pady=10)
 
 def scdl_thu():
     scdl_day_btn_mon.destroy()
@@ -295,8 +302,8 @@ root.title("Menu")
 root.attributes("-zoomed", "1")
 
 # あとで表示するオブジェクト
-scdl_finish_btn1 = tk.Button(root, text="編集完了", font=("", 25), command=scdl_finish)
-scdl_finish_btn2 = tk.Button(root, text="キャンセル", font=("", 25), command=scdl_cancel)
+scdl_finish_btn = tk.Button(root, text="編集完了", font=("", 25), command=scdl_finish)
+scdl_cancel_btn = tk.Button(root, text="キャンセル", font=("", 25), command=scdl_cancel)
 
 scdl_mon2_btn1 = tk.Button(root, text="工学数理演習", font=("", 25), command=scdl_mon_finish)
 scdl_mon2_btn2 = tk.Button(root, text="社会と工学", font=("", 25), command=scdl_mon_finish)
@@ -321,7 +328,7 @@ scdl_fri2_btn4 = tk.Button(root, text="電子機械設計製作", font=("", 25),
 
 scdl_day2_btn_mon = tk.Button(root, text="月曜日", font=("", 25), command=scdl_mon2)
 scdl_day2_btn_tue = tk.Button(root, text="火曜日", font=("", 25), command=scdl_tue2)
-scdl_day2_btn_wen = tk.Button(root, text="水曜日", font=("", 25), command=scdl_wen2)
+scdl_day2_btn_wed = tk.Button(root, text="水曜日", font=("", 25), command=scdl_wed2)
 scdl_day2_btn_thu = tk.Button(root, text="木曜日", font=("", 25), command=scdl_thu2)
 scdl_day2_btn_fri = tk.Button(root, text="金曜日", font=("", 25), command=scdl_fri2)
 
@@ -357,7 +364,7 @@ scdl_fri_btn4 = tk.Button(root, text="電子機械設計製作", font=("", 25), 
 
 scdl_day_btn_mon = tk.Button(root, text="月曜日", font=("", 25), command=scdl_mon)
 scdl_day_btn_tue = tk.Button(root, text="火曜日", font=("", 25), command=scdl_tue)
-scdl_day_btn_wen = tk.Button(root, text="水曜日", font=("", 25), command=scdl_wen)
+scdl_day_btn_wen = tk.Button(root, text="水曜日", font=("", 25), command=scdl_wed)
 scdl_day_btn_thu = tk.Button(root, text="木曜日", font=("", 25), command=scdl_thu)
 scdl_day_btn_fri = tk.Button(root, text="金曜日", font=("", 25), command=scdl_fri)
 
